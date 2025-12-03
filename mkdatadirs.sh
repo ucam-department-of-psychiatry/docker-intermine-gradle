3a3c9e795553e5770d4241209c4beac43b30c704
Martin Burchell <mb2353@cam.ac.uk>
2025-12-03 10:43:27 +0000

    Add Bluegenes to the Docker compose file

--- mkdatadirs.sh
+++ mkdatadirs.sh
@@ -1,15 +1,18 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-THIS_DIR=$(dirname $0)
+set -euo pipefail
+
+THIS_DIR=$(dirname "$0")
 
 mkdir -p \
-    ${THIS_DIR}/data/mine/dump \
-    ${THIS_DIR}/data/mine/configs \
-    ${THIS_DIR}/data/mine/packages \
-    ${THIS_DIR}/data/mine/intermine \
-    ${THIS_DIR}/data/postgres \
-    ${THIS_DIR}/data/solr
+    "${THIS_DIR}"/data/bluegenes_tools \
+    "${THIS_DIR}"/data/mine/dump \
+    "${THIS_DIR}"/data/mine/configs \
+    "${THIS_DIR}"/data/mine/packages \
+    "${THIS_DIR}"/data/mine/intermine \
+    "${THIS_DIR}"/data/postgres \
+    "${THIS_DIR}"/data/solr
 
-sudo chown -R 8983:8983 ${THIS_DIR}/data/solr
+sudo chown -R 8983:8983 "${THIS_DIR}"/data/solr
 
 echo "Don't forget to also mkdir ${THIS_DIR}/data/mine/<your mine name>"
